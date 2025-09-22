b44f482126a60d00d2f1453be9b93de587e0d443
Svenja Borchert <svenja@positive-games.com>
2025-09-22 11:09:10 +0200

    Implemented paper drag and drop system

---
+++ EQ_SeatingChart/Assets/Scripts/PaperObject.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PaperObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    private RectTransform paperObjectRectTransform;
+    [SerializeField]
+    private CanvasGroup paperObjectCanvasGroup;
+    [SerializeField]
+    private Canvas backgroundCanvas;
+
+    private Vector2 originalPosition;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalPosition = this.paperObjectRectTransform.anchoredPosition;
+        
+        // UX
+        this.paperObjectCanvasGroup.alpha = 0.8f;
+        
+        this.paperObjectCanvasGroup.blocksRaycasts = false;
+
+        // Bring to front
+        this.transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Move with cursor, adjusted for canvas scaling
+        this.paperObjectRectTransform.anchoredPosition += eventData.delta / this.backgroundCanvas.scaleFactor;
+    } 
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // UX
+        this.paperObjectCanvasGroup.alpha = 1f;
+        
+        this.paperObjectCanvasGroup.blocksRaycasts = true;
+    }
+}
+
+    
+
